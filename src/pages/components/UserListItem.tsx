3b543c265f05a6f6d22bbafdd547bf9aef3cc22d
Nahom Habtamu <nahomhab2626@gmail.com>
2024-02-02 10:33:13 +0300

    style: add style fixes to make card look better

--- src/pages/components/UserListItem.tsx
+++ src/pages/components/UserListItem.tsx
@@ -12,14 +12,14 @@ type User = {
 
 const UserListItem = (user: User) => {
   return (
-    <div className="max-w-lg rounded overflow-hidden shadow-lg">
+    <div className="max-w-lg rounded overflow-hidden shadow-lg p-3">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">
           Name: {user.firstName + " " + user.lastName}
         </div>
-        <p className="text-gray-700 text-base p-2">Email : {user.email}</p>
-        <p className="text-gray-700 text-base p-2">Country : {user.country}</p>
-        <p className="text-gray-700 text-base p-2">City : {user.city}</p>
+        <p className="text-gray-700 text-base pt-2">Email : {user.email}</p>
+        <p className="text-gray-700 text-base pt-2">Country : {user.country}</p>
+        <p className="text-gray-700 text-base pt-2">City : {user.city}</p>
       </div>
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
