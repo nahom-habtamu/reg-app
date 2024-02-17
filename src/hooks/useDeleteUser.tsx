8baa1b7840d3f68e4ad2c916d96905748dc4d8f3
Nahom Habtamu <nahomhab2626@gmail.com>
2024-02-17 14:31:10 +0300

    refactor: useDeleteUser hook

--- src/hooks/useDeleteUser.tsx
+++ src/hooks/useDeleteUser.tsx
@@ -3,19 +3,21 @@ import { useMutation, useQueryClient } from "react-query";
 
 export default function useDeletePost() {
   const queryClient = useQueryClient();
-
-  const { isSuccess, mutate, isLoading } = useMutation({
+  const {
+    isSuccess,
+    mutate: deleteUser,
+    isLoading,
+  } = useMutation({
     mutationFn: async (userId: string) => {
       return await axios.delete(`/api/users/${userId}`).then((res) => res.data);
     },
     onSuccess: () => {
       queryClient.invalidateQueries("users");
-      alert("DELETED POST");
     },
   });
 
   return {
-    deleteUser: mutate,
+    deleteUser,
     isLoading,
     isSuccess,
   };
